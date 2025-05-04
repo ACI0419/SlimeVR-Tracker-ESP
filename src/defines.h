e2203c4550d42ec1c5aaa71bab70615dcb8dbb46
ACI0419 <UVOEOO8@outlook.com>
2025-05-04 21:48:07 +0800

    修改define.h适配单ICM45686

--- src/defines.h
+++ src/defines.h
@@ -26,9 +26,9 @@
 // ================================================
 
 // Set parameters of IMU and board used
-#define IMU IMU_BNO085
+#define IMU IMU_ICM45686
 #define SECOND_IMU IMU
-#define BOARD BOARD_SLIMEVR
+#define BOARD BOARD_SLIMEVR_DEV
 #define IMU_ROTATION DEG_270
 #define SECOND_IMU_ROTATION DEG_270
 
