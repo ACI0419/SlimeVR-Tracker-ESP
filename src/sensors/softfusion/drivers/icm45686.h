2aa5a38d86191f3301199a22eb43b5f24c8a97f8
ACI0419 <UVOEOO8@outlook.com>
2025-06-13 14:33:54 +0800

    适配45686模块

--- src/sensors/softfusion/drivers/icm45686.h
+++ src/sensors/softfusion/drivers/icm45686.h
@@ -64,7 +64,7 @@ struct ICM45686 : public ICM45Base<I2CImpl> {
 
 		struct RtcConfig {
 			static constexpr uint8_t reg = 0x26;
-			static constexpr uint8_t value = 0b00100011;  // enable RTC
+			static constexpr uint8_t value = 0b00000000;  // enable RTC
 		};
 	};
 
